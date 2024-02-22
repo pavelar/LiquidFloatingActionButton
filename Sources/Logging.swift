759e3993d895b26085d85755d0e24f191c75d5cd
Phillip Avelar <36336702+pavelar@users.noreply.github.com>
2024-02-22 15:44:25 -0600

    Add Unified Logging

--- Sources/Logging.swift
+++ Sources/Logging.swift
@@ -5,4 +5,7 @@
 //  Created by Phillip Avelar on 2/22/24.
 //
 
-import Foundation
+import OSLog
+
+// Define the logger with internal access
+internal let logger = Logger(subsystem: "com.AdvSolutionPros", category: "AdvSSOKit")
