617dd44f4b24084ebdb5fe5738378d773dea0650
Phillip Avelar <36336702+pavelar@users.noreply.github.com>
2024-02-22 18:28:42 -0600

    Update SPM

--- Package.swift
+++ Package.swift
@@ -22,8 +22,5 @@ let package = Package(
             name: "LiquidFloatingActionButton",
             dependencies: []),
             
-        .testTarget(
-            name: "LiquidFloatingActionButtonTests",
-            dependencies: ["LiquidFloatingActionButton"]),
     ]
 )
